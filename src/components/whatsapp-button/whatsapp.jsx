c14714ba331015b212725852cb584924e6e3b4ad
Aguinaldo Konzo <akakkonzo38@gmail.com>
2026-09-26 13:00:12 +0000

    atualizacao website

--- src/components/whatsapp-button/whatsapp.jsx
+++ src/components/whatsapp-button/whatsapp.jsx
@@ -2,8 +2,8 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./whatsappButton.css";
 
 function WhatsappButton() {
-    const phone = "244925524048"; // Código do país + número
-    const message = "Olá! Vim através do site da Mundo Clean e gostaria de falar com um consultor para conhecer melhor os serviços e soluções disponíveis.";
+    const phone = "244922116411"; // Código do país + número
+    const message = "Olá! Encontrei a [Nome da Lavandaria] através do vosso site e gostaria de saber mais sobre os serviços disponíveis, preços e condições de atendimento. Poderiam indicar-me um consultor?";
 
     return (
         <a
